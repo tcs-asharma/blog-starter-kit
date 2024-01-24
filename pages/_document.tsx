bd3ae2950d032e9773be5ea2d80ba8ff7e4f8c05
Akshay Sharma <86650974+tcs-asharma@users.noreply.github.com>
2024-01-24 20:40:02 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   if (['header'].includes(this.props.__NEXT_DATA__.props.pageProps.page)) {
+      console.log('inside here..');
+      
       return <Main />;
-    }
+  }
   
   return (
     <Html lang="en">
