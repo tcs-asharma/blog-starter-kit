e58de5daa4c2db85a1a09f971b87831a9c5c772d
Akshay Sharma <86650974+tcs-asharma@users.noreply.github.com>
2024-01-24 20:42:00 +0530

    Update and rename header to header.js

---
+++ pages/api/script/header.js
@@ -0,0 +1,18 @@
+import fs from "fs"
+import path from  "path";
+ 
+export default async function handler(req, res) {
+  
+  try {
+ 
+    const filePath = path.resolve('.next', 'build-manifest.json');
+    const buffer = fs.readFileSync(filePath);
+    const response = buffer;
+    const json = JSON.parse(buffer.toString());
+    res.status(200).json(json);
+  } catch (error) {
+    res
+    .status(error.response?.status || '500')
+    .json({ message: error });
+  }
+}
