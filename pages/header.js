be3fd702c3b056ef3fe26310fca69b1ef6144a61
Akshay Sharma <86650974+tcs-asharma@users.noreply.github.com>
2024-01-24 22:28:39 +0530

    Update header.js

--- pages/header.js
+++ pages/header.js
@@ -1,1 +1,46 @@
+import { useEffect } from 'react';
+import Container from "../components/container";
+import MoreStories from "../components/more-stories";
+import HeroPost from "../components/hero-post";
+import Intro from "../components/intro";
+import Layout from "../components/layout";
+import { getAllPosts } from "../lib/api";
+import Head from "next/head";
+import { CMS_NAME } from "../lib/constants";
+import Post from "../interfaces/post";
 
+type Props = {
+  allPosts: Post[];
+};
+
+export default function Index({ allPosts }: Props) {
+  useEffect(() => {
+    document.getElementById('header').style.color = 'red';
+  }, []);
+  
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
+  return (
+    <h1 id="header">
+      Header from NExtJS app
+    </h1>
+  );
+}
+
+export const getStaticProps = async () => {
+  const allPosts = getAllPosts([
+    "title",
+    "date",
+    "slug",
+    "author",
+    "coverImage",
+    "excerpt",
+  ]);
+
+  return {
+    props: {
+      allPosts,
+      page: 'header-new',
+    },
+  };
+};
